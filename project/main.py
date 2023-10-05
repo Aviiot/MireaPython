fa5d6cd03093efb9a5530e5e464eed2c909a9911
Aviiot <147001696+Aviiot@users.noreply.github.com>
2023-10-05 16:34:36 +0300

    Add example pygame

--- project/main.py
+++ project/main.py
@@ -1,5 +1,26 @@
-def foo():
-    pass
+import pygame
+def main():
+    pygame.init()
+
+    width = 800
+    height = 600
+
+    window = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("")
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        window.fill((255, 255, 255))
+
+        pygame.display.flip()
+
+
+    pygame.quit()
+
 
 if __name__ == '__main__':
     main()
