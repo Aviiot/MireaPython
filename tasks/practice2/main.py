3802908f9e77784298fd510f3cea823b91a52db1
Aviiot <147001696+Aviiot@users.noreply.github.com>
2023-10-05 13:29:26 +0300

    Add function digit_sum for task2

--- tasks/practice2/main.py
+++ tasks/practice2/main.py
@@ -7,6 +7,17 @@ def task1():
 	calculate_coordinate_dot(2, 5, 10, 3) # 5 + 10 * 2 + (3 * 2 ** 2) / 2 = 5 + 20 + 6 = 31
 	calculate_coordinate_dot(4, -2, 0, -5) # -2 + 0 * 4 + (-5 * 4 ** 2) / 2 = -2 + 0 + (-5 * 16) / 2 = -2 - 40 = -42
 	calculate_coordinate_dot(3.5) # 0 + 0 * 3.5 + (0 * 3.5 ** 2) / 2 = 0
+def task2():
+	def digit_sum(number):
+		digit_sum = 0
+		while number != 0:
+			digit_sum += number % 10
+			number //= 10
+		print(digit_sum)
+	digit_sum(int(input()))
+
+
+
 
 def main():
 	main_window = tk.Tk()
@@ -18,9 +29,9 @@ def main():
 	try:
 		tasks = [
 			("Задание №1", task1),
-			"""("Задание №2", task2),
+			("Задание №2", task2),
 			("Задание №3", task3),
-			("Задание №4", task4),
+			"""("Задание №4", task4),
 			("Задание №5", task5),
 			("Задание №6", task6),
 			("Задание №7", task7) """
