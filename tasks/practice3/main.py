655ff8aa3a1cb1308f6b863a9ff3f7722f708303
Aviiot <147001696+Aviiot@users.noreply.github.com>
2023-11-17 16:34:02 +0300

    Task1 ready

--- tasks/practice3/main.py
+++ tasks/practice3/main.py
@@ -1,4 +1,13 @@
-def foo():
-    pass
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, world!'
+
+
 if __name__ == '__main__':
-    foo()
+    app.run()
