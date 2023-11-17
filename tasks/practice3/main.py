bd668186221fd22c98c37aedb3461bd375e71bb1
Aviiot <147001696+Aviiot@users.noreply.github.com>
2023-11-17 16:58:54 +0300

    Task 2,3 completed

--- tasks/practice3/main.py
+++ tasks/practice3/main.py
@@ -1,13 +1,49 @@
-from flask import Flask
+def task1():
+
+    from flask import Flask
 
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, world!'
+    @app.route('/')
+    def hello_world():
+        return 'Hello, world!'
 
 
-if __name__ == '__main__':
-    app.run()
+    if __name__ == '__main__':
+        app.run()
+
+def task2():
+    with open('output.txt', 'w') as file:
+        while True:
+            input_str = input("Введите строку")
+            if input_str.lower() == 'стоп':
+                break
+            file.write(input_str + '\n')
+
+def task3():
+    from flask import Flask, render_template, request
+
+    app = Flask(__name__)
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    @app.route('/add_to_file', methods=['POST'])
+    def add_to_file():
+        input_str = request.form['input_str']
+        if input_str.lower() != 'стоп':
+            with open('output.txt', 'a') as file:
+                file.write(input_str + '\n')
+        return render_template('index.html')
+
+    @app.route('/view_file')
+    def view_file():
+        content = []
+        with open('output.txt', 'r') as file:
+            content = file.readlines()
+        return render_template('view_file.html', content=content)
+
+    if __name__ == '__main__':
+        app.run()
+task4()
